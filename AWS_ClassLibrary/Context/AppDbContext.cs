f8708ca776289732e35f303befa889a2270fd380
Francisco Carmona <flcarmonac@yahoo.com>
2025-08-23 01:55:56 -0400

    update

--- AWS_ClassLibrary/Context/AppDbContext.cs
+++ AWS_ClassLibrary/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using AWS_ClassLibrary.Entities;
+﻿using AWS_ClassLibrary.DTOs;
+using AWS_ClassLibrary.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace AWS_ClassLibrary.Context;
@@ -10,13 +11,29 @@ public class AppDbContext : DbContext
     public DbSet<Company> companies { get; set; }
     public DbSet<Product> products { get; set; }
 
-    public async Task<IEnumerable<Company>> GetCompanies()
+    public async Task<IEnumerable<CompanyDTO>> GetCompanies()
     {
-        return await this.companies.ToListAsync();
+        var entity = await companies.ToListAsync();
+
+        return entity.Select(e => new CompanyDTO
+        {
+            Company_id = e.Company_id,
+            Name = e.Name,
+            Email = e.Email,
+            Img = e.Img
+        });
     }
 
-    public async Task<IEnumerable<Product>> GetProducts()
+    public async Task<IEnumerable<ProductDTO>> GetProducts()
     {
-        return await this.products.ToListAsync();
+        var entity = await products.ToListAsync();
+
+        return entity.Select(e => new ProductDTO
+        {
+            Product_id = e.Product_id,
+            Name = e.Name,
+            Description = e.Description,
+            Price = e.Price
+        });
     }
 }
